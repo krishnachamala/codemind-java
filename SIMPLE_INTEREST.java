836a8f92cd1398cd191fb6e766bb2783988e72fc
krishnachamala <84897549+krishnachamala@users.noreply.github.com>
2022-10-14 11:00:16 +0530

    import java.util.*; class d {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int p,t,r,s;         p=sc.nextInt();         t=sc.nextInt();         r=sc.nextInt();         s=(p*t*r)/100;         System.out.println(s);
        }
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class d
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int p,t,r,s;
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        s=(p*t*r)/100;
+        System.out.println(s);
+        
+    }
+}
